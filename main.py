0b26c51e4e6d088b4d1c125a82af02e5a48ba8b8
scott <haha@skdjksd.sjkdsjd>
2023-12-08 13:01:09 -0800

    print length of file

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
-def print_hi(name):
-    print(f'Hi, {name}')
-
-
 def get_bakery_file():
     sales_data_file = open('Bakery.csv', 'r')
     sales_list = []
     for singleSale in sales_data_file:
         sales_list.append(singleSale)
     sales_data_file.close()
+    print("lines in file=" + str(len(sales_list)))
 
 
 if __name__ == '__main__':
